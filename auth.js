058d60da525b0bfa549abd5d8e856ae39211be44
Atlas Soft Home <33475201+EngMohamed3382@users.noreply.github.com>
2025-10-02 10:55:47 +0300

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,7 +1,6 @@
 // استبدل التالي بالمعلومات الخاصة بمشروعك في Supabase
-const SUPABASE_URL = 'https://mzhwuoyetkymfjmzrufp.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16aHd1b3lldGt5bWZqbXpydWZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzNzk5NDksImV4cCI6MjA3NDk1NTk0OX0.8lXagi3kauUVkJag7S2I93t52-QaFTVs9k4SrImqiRE';
-
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
 
 // تهيئة Supabase
 // تم تغيير اسم المتغير إلى supabaseClient لتجنب التضارب
